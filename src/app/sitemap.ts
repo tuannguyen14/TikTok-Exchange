e49ee922c6a11b84a9fa0c3ee959ba7fa8fc7fcf
Tuấn++ <tuannguyen7067@gmail.com>
2025-07-26 14:08:12 +0700

    Update
    sitemaps
    robots

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+// app/sitemap.ts
+import { MetadataRoute } from 'next'
+
+const languages = ['vi', 'en']
+const pages = [
+  'tiktok-exchange-followers-likes',
+  'get-tiktok-followers-likes',
+  'campaigns',
+  'profile'
+]
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://tikgrow.io'
+
+  const routes = languages.flatMap((lang) =>
+    pages.map((page) => ({
+      url: `${baseUrl}/${lang}/${page}`,
+      lastModified: new Date(),
+      // optional: changefreq, priority
+    }))
+  )
+
+  return routes
+}
